175afd7217b5d708c15c8b1913707d7aef057e13
Sadiyahmed123 <84186677+Sadiyahmed123@users.noreply.github.com>
2022-02-25 18:41:01 +0530

    import java.util.Scanner; class Inp{     public static void main(String args[])     {         String str;         Scanner s=new Scanner(System.in);         str=s.nextLine();         System.out.println(str);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Inp{
+    public static void main(String args[])
+    {
+        String str;
+        Scanner s=new Scanner(System.in);
+        str=s.nextLine();
+        System.out.println(str);
+    }
+}
